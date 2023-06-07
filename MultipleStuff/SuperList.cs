b9e251405aa0df96308d01fcd3fc1f82b98b48a2
Vitor <vitor.daniel@outlook.pt>
2023-06-07 15:13:04 +0100

    add method GetMinMax1

--- MultipleStuff/SuperList.cs
+++ MultipleStuff/SuperList.cs
@@ -5,8 +5,26 @@ using System.Threading.Tasks;
 
 namespace MultipleStuff
 {
-    public class SuperList
+
+    public class SuperList : List<double>
     {
-        
+        // Constructor for SuperList
+        public SuperList(IEnumerable<double> collection) : base(collection)
+        {
+        }
+
+        // Method to get the minimum and maximum values using out parameters
+        public (double Min, double Max) GetMinMax1()
+        {
+            double min = double.MaxValue;
+            double max = double.MinValue;
+
+            foreach (var item in this)
+            {
+                UpdateMinMax(out min, out max, item);
+            }
+
+            return (Min: min, Max: max);
+        }
     }
 }
